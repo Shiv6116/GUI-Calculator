3931f847504005bf0a54789b142bc834db6731f0
SHIV <128344968+Shiv6116@users.noreply.github.com>
2024-02-12 23:29:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,219 +6,219 @@ class Ui_Calculator(object):
         self.stack = []
     def setupUi(self, Calculator):
         Calculator.setObjectName("Calculator")
-        Calculator.resize(269, 502)
+        Calculator.resize(280, 502)
         Calculator.setStyleSheet("QWidget{\n"
-                                 "background-color: rgb(0, 0, 0);\n"
+                                 "background-color: rgb(0,0,0);\n"
                                  "border-radius: 20px;\n"
                                  "}\n"
                                  "")
         self.display = QtWidgets.QLabel(Calculator)
         self.display.setGeometry(QtCore.QRect(0, 0, 271, 171))
         self.display.setStyleSheet("QLabel{\n"
-                                   "background-color: rgb(46, 52, 54);\n"
+                                   "background-color: rgb(0, 0, 0);\n"
                                    "border-radius: 20px;\n"
                                    "}")
         self.display.setObjectName("display")
         self.AC = QtWidgets.QPushButton(Calculator)
         self.AC.setGeometry(QtCore.QRect(10, 200, 111, 51))
         self.AC.setStyleSheet("QPushButton {\n"
-                              "    background-color: rgb(57, 67, 78);\n"
+                              "    background-color: rgb(57, 97, 78);\n"
                               "    border-radius: 24px;                   \n"
                               "    color: rgb(0, 230, 255);   }\n"
                               "\n"
                               "QPushButton:hover {\n"
-                              "    background: rgb(46, 52, 54);           /* Hover background color */\n"
+                              "    background: rgb(255, 52, 54);           /* Hover background color */\n"
                               "}")
         self.AC.setObjectName("AC")
         self.but7 = QtWidgets.QPushButton(Calculator)
         self.but7.setGeometry(QtCore.QRect(10, 260, 51, 51))
         self.but7.setStyleSheet("QPushButton {\n"
-                                "    background-color: rgb(57, 67, 78);\n"
+                                "    background-color: rgb(57, 97, 78);\n"
                                 "    border-radius: 24px;                   \n"
                                 "    color: rgb(0, 230, 255);   }\n"
                                 "\n"
                                 "QPushButton:hover {\n"
-                                "    background: rgb(46, 52, 54);           /* Hover background color */\n"
+                                "    background: rgb(255, 52, 54);           /* Hover background color */\n"
                                 "}")
         self.but7.setObjectName("but7")
         self.but0 = QtWidgets.QPushButton(Calculator)
         self.but0.setGeometry(QtCore.QRect(10, 440, 111, 51))
         self.but0.setStyleSheet("QPushButton {\n"
-                                "    background-color: rgb(57, 67, 78);\n"
+                                "    background-color: rgb(57, 97, 78);\n"
                                 "    border-radius: 24px;                   \n"
                                 "   color: rgb(0, 230, 255);    }\n"
                                 "\n"
                                 "QPushButton:hover {\n"
-                                "    background: rgb(46, 52, 54);           /* Hover background color */\n"
+                                "    background: rgb(255, 52, 54);           /* Hover background color */\n"
                                 "}")
         self.but0.setObjectName("but0")
         self.but5 = QtWidgets.QPushButton(Calculator)
         self.but5.setGeometry(QtCore.QRect(70, 320, 51, 51))
         self.but5.setStyleSheet("QPushButton {\n"
-                                "    background-color: rgb(57, 67, 78);\n"
+                                "    background-color: rgb(57, 97, 78);\n"
                                 "    border-radius: 24px;                   \n"
                                 "    color: rgb(0, 230, 255);    }\n"
                                 "\n"
                                 "QPushButton:hover {\n"
-                                "    background: rgb(46, 52, 54);           /* Hover background color */\n"
+                                "    background: rgb(255, 52, 54);           /* Hover background color */\n"
                                 "}")
         self.but5.setObjectName("but5")
         self.but6 = QtWidgets.QPushButton(Calculator)
         self.but6.setGeometry(QtCore.QRect(130, 320, 51, 51))
         self.but6.setStyleSheet("QPushButton {\n"
-                                "    background-color: rgb(57, 67, 78);\n"
+                                "    background-color: rgb(57, 97, 78);\n"
                                 "    border-radius: 24px;                   \n"
                                 "    color: rgb(0, 230, 255);    }\n"
                                 "\n"
                                 "QPushButton:hover {\n"
-                                "    background: rgb(46, 52, 54);           /* Hover background color */\n"
+                                "    background: rgb(255, 52, 54);           /* Hover background color */\n"
                                 "}")
         self.but6.setObjectName("but6")
         self.but1 = QtWidgets.QPushButton(Calculator)
         self.but1.setGeometry(QtCore.QRect(10, 380, 51, 51))
         self.but1.setStyleSheet("QPushButton {\n"
-                                "    background-color: rgb(57, 67, 78);\n"
+                                "    background-color: rgb(57, 97, 78);\n"
                                 "    border-radius: 24px;                   \n"
                                 "    color: rgb(0, 230, 255);  }\n"
                                 "\n"
                                 "QPushButton:hover {\n"
-                                "    background: rgb(46, 52, 54);           /* Hover background color */\n"
+                                "    background: rgb(255, 52, 54);           /* Hover background color */\n"
                                 "}")
         self.but1.setObjectName("but1")
         self.but4 = QtWidgets.QPushButton(Calculator)
         self.but4.setGeometry(QtCore.QRect(10, 320, 51, 51))
         self.but4.setStyleSheet("QPushButton {\n"
-                                "    background-color: rgb(57, 67, 78);\n"
+                                "    background-color: rgb(57, 97, 78);\n"
                                 "    border-radius: 24px;                   \n"
                                 "   color: rgb(0, 230, 255);    }\n"
                                 "\n"
                                 "QPushButton:hover {\n"
-                                "    background: rgb(46, 52, 54);           /* Hover background color */\n"
+                                "    background: rgb(255, 52, 54);           /* Hover background color */\n"
                                 "}")
         self.but4.setObjectName("but4")
         self.but2 = QtWidgets.QPushButton(Calculator)
         self.but2.setGeometry(QtCore.QRect(70, 380, 51, 51))
         self.but2.setStyleSheet("QPushButton {\n"
-                                "    background-color: rgb(57, 67, 78);\n"
+                                "    background-color: rgb(57, 97, 78);\n"
                                 "    border-radius: 24px;                   \n"
                                 "   color: rgb(0, 230, 255);     }\n"
                                 "\n"
                                 "QPushButton:hover {\n"
-                                "    background: rgb(46, 52, 54);           /* Hover background color */\n"
+                                "    background: rgb(255, 52, 54);           /* Hover background color */\n"
                                 "}")
         self.but2.setObjectName("but2")
         self.but8 = QtWidgets.QPushButton(Calculator)
         self.but8.setGeometry(QtCore.QRect(70, 260, 51, 51))
         self.but8.setStyleSheet("QPushButton {\n"
-                                "    background-color: rgb(57, 67, 78);\n"
+                                "    background-color: rgb(57, 97, 78);\n"
                                 "    border-radius: 24px;                   \n"
                                 "    color: rgb(0, 230, 255);  }\n"
                                 "\n"
                                 "QPushButton:hover {\n"
-                                "    background: rgb(46, 52, 54);           /* Hover background color */\n"
+                                "    background: rgb(255, 52, 54);           /* Hover background color */\n"
                                 "}")
         self.but8.setObjectName("but8")
         self.but3 = QtWidgets.QPushButton(Calculator)
         self.but3.setGeometry(QtCore.QRect(130, 380, 51, 51))
         self.but3.setStyleSheet("QPushButton {\n"
-                                "    background-color: rgb(57, 67, 78);\n"
+                                "    background-color: rgb(57, 97, 78);\n"
                                 "    border-radius: 24px;                   \n"
                                 "    color: rgb(0, 230, 255);    }\n"
                                 "\n"
                                 "QPushButton:hover {\n"
-                                "    background: rgb(46, 52, 54);           /* Hover background color */\n"
+                                "    background: rgb(255, 52, 54);           /* Hover background color */\n"
                                 "}")
         self.but3.setObjectName("but3")
         self.but9 = QtWidgets.QPushButton(Calculator)
         self.but9.setGeometry(QtCore.QRect(130, 260, 51, 51))
         self.but9.setStyleSheet("QPushButton {\n"
-                                "    background-color: rgb(57, 67, 78);\n"
+                                "    background-color: rgb(57, 97, 78);\n"
                                 "    border-radius: 24px;                   \n"
                                 "    color: rgb(0, 230, 255); }\n"
                                 "\n"
                                 "QPushButton:hover {\n"
-                                "    background: rgb(46, 52, 54);           /* Hover background color */\n"
+                                "    background: rgb(255, 52, 54);           /* Hover background color */\n"
                                 "}")
         self.but9.setObjectName("but9")
         self.butdot = QtWidgets.QPushButton(Calculator)
         self.butdot.setGeometry(QtCore.QRect(130, 440, 51, 51))
         self.butdot.setStyleSheet("QPushButton {\n"
-                                  "    background-color: rgb(57, 67, 78);\n"
+                                  "    background-color: rgb(57, 97, 78);\n"
                                   "    border-radius: 24px;                   \n"
                                   "    color: rgb(0, 230, 255);   }\n"
                                   "\n"
                                   "QPushButton:hover {\n"
-                                  "    background: rgb(46, 52, 54);           /* Hover background color */\n"
+                                  "    background: rgb(255, 52, 54);           /* Hover background color */\n"
                                   "}")
         self.butdot.setObjectName("butdot")
         self.percent = QtWidgets.QPushButton(Calculator)
         self.percent.setGeometry(QtCore.QRect(130, 200, 51, 51))
         self.percent.setStyleSheet("QPushButton {\n"
-                                   "    background-color: rgb(57, 67, 78);\n"
+                                   "    background-color: rgb(57, 97, 78);\n"
                                    "    border-radius: 24px;                   \n"
                                    "    color: rgb(0, 230, 255);   }\n"
                                    "\n"
                                    "QPushButton:hover {\n"
-                                   "    background: rgb(46, 52, 54);           /* Hover background color */\n"
+                                   "    background: rgb(255, 52, 54);           /* Hover background color */\n"
                                    "}")
         self.percent.setObjectName("percent")
         self.divide = QtWidgets.QPushButton(Calculator)
         self.divide.setGeometry(QtCore.QRect(200, 200, 61, 51))
         self.divide.setStyleSheet("QPushButton {\n"
-                                  "    background-color: rgb(57, 67, 78);             \n"
+                                  "    background-color: rgb(57, 97, 78);             \n"
                                   "    color: rgb(0, 230, 255);   }\n"
                                   "\n"
                                   "QPushButton:hover {\n"
-                                  "    background: rgb(46, 52, 54);  \n"
+                                  "    background: rgb(255, 52, 54);  \n"
                                   "}")
         self.divide.setObjectName("divide")
         self.multiply = QtWidgets.QPushButton(Calculator)
         self.multiply.setGeometry(QtCore.QRect(200, 260, 61, 51))
         self.multiply.setStyleSheet("QPushButton {\n"
-                                    "    background-color: rgb(57, 67, 78);            \n"
+                                    "    background-color: rgb(57, 97, 78);            \n"
                                     "    color: rgb(0, 230, 255);   }\n"
                                     "\n"
                                     "QPushButton:hover {\n"
-                                    "    background: rgb(46, 52, 54);           /* Hover background color */\n"
+                                    "    background: rgb(255, 52, 54);           /* Hover background color */\n"
                                     "}")
         self.multiply.setObjectName("multiply")
         self.minus = QtWidgets.QPushButton(Calculator)
         self.minus.setGeometry(QtCore.QRect(200, 320, 61, 51))
         self.minus.setStyleSheet("QPushButton {\n"
-                                 "    background-color: rgb(57, 67, 78);                 \n"
+                                 "    background-color: rgb(57, 97, 78);                 \n"
                                  "    color: rgb(0, 230, 255);   }\n"
                                  "\n"
                                  "QPushButton:hover {\n"
-                                 "    background: rgb(46, 52, 54);           /* Hover background color */\n"
+                                 "    background: rgb(255, 52, 54);           /* Hover background color */\n"
                                  "}")
         self.minus.setObjectName("minus")
         self.plus = QtWidgets.QPushButton(Calculator)
         self.plus.setGeometry(QtCore.QRect(200, 380, 61, 51))
         self.plus.setStyleSheet("QPushButton {\n"
-                                "    background-color: rgb(57, 67, 78);           \n"
+                                "    background-color: rgb(57, 97, 78);           \n"
                                 "    color: rgb(0, 230, 255);   }\n"
                                 "\n"
                                 "QPushButton:hover {\n"
-                                "    background: rgb(46, 52, 54);           /* Hover background color */\n"
+                                "    background: rgb(255, 52, 54);           /* Hover background color */\n"
                                 "}")
         self.plus.setObjectName("plus")
         self.equals = QtWidgets.QPushButton(Calculator)
         self.equals.setGeometry(QtCore.QRect(200, 440, 61, 51))
         self.equals.setStyleSheet("QPushButton {\n"
-                                  "    background-color: rgb(57, 67, 78);                \n"
+                                  "    background-color: rgb(57, 97, 78);                \n"
                                   "    color: rgb(0, 230, 255);   }\n"
                                   "\n"
                                   "QPushButton:hover {\n"
-                                  "    background: rgb(46, 52, 54);           /* Hover background color */\n"
+                                  "    background: rgb(255, 52, 54);           /* Hover background color */\n"
                                   "}")
         self.equals.setObjectName("equals")
         self.label = QtWidgets.QLabel(Calculator)
-        self.label.setGeometry(QtCore.QRect(0, 40, 271, 51))
+        self.label.setGeometry(QtCore.QRect(0, 50, 271, 51))
         self.label.setStyleSheet("")
         self.label.setObjectName("label")
         self.output = QtWidgets.QLabel(Calculator)
         self.output.setGeometry(QtCore.QRect(150, 116, 111, 41))
-        self.output.setStyleSheet("background-color: rgb(46, 52, 54);")
+        self.output.setStyleSheet("background-color: rgb(0, 0, 0);")
         self.output.setObjectName("outpt")
         self.line = QtWidgets.QFrame(Calculator)
         self.line.setGeometry(QtCore.QRect(0, 180, 271, 2))
